079b6d75f94f8938cb7a2fe756be005054403594
cfchape97 <115433943+cfchape97@users.noreply.github.com>
2023-04-03 22:23:06 -0400

    Add files via upload

---
+++ DailyRoutine2.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const addBtn = document.querySelector('#add');
+addBtn.addEventListener('click', function() {
+    const div = document.createElement('div');
+    div.innerText = document.querySelector('#habit').value;
+    div.setAttribute('class', 'checkbox');
+    body.appendChild(div);
+
+    const checkBtn = document.createElement('button');
+    checkBtn.setAttribute('class','fa-solid fa-check');
+    checkBtn.classList.add('checkBtn');
+    div.appendChild(checkBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'fa-regular fa-circle-xmark');
+    deleteBtn.classList.add('deleteBtn');
+    div.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', function() {
+        div.remove(div.removeChild);
+    })
+
+    checkBtn.addEventListener('click',function() {
+        div.style.textDecoration = 'line-through';
+    })
+
+    
+})
+
